1c6f0621f0f7503ed3559b43f84ee1e9bc6554ac
viniciusjn <viniciusaqpsjn@gmail.com>
2024-04-03 21:41:04 -0300

    commit final

--- trabalho_01.Vetores/src/main/ex04.java
+++ trabalho_01.Vetores/src/main/ex04.java
@@ -1,6 +1,19 @@
 // Vetor com tamanho dinâmico. Implemente um vetor que aumente de tamanho dinamicamente à medida que elementos são adicionados a ele. Inicie esse vetor com tamanho 0. Depois, adicione os seguintes itens (um a um): {1, 2, 3, 4, 5}
 
 package main;
+import java.util.ArrayList;
 
 public class ex04 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> vetor = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            vetor.add(i);
+        }
+
+        for (int elemento : vetor) {
+            System.out.println(elemento);
+        }
+    }
 }
--- trabalho_01.Vetores/src/main/ex05.java
+++ trabalho_01.Vetores/src/main/ex05.java
@@ -1,6 +1,22 @@
 // Utilize o vetor de tamanho dinâmico criado anteriormente. Gere 1000 números aleatórios e armazene neste vetor.
 
 package main;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ex05 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> vetor = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 1000; i++) {
+            vetor.add(random.nextInt());
+        }
+        
+        for (int i = 0; i < 20; i++) {
+            System.out.println(vetor.get(i));
+        }
+    }
 }
